370fbeace3d104f1e411f9fe953753de823ba1b5
Ayush <aayush.kamble9b@gmail.com>
2026-06-19 12:07:53 +0530

    Created a Re-Usable Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom"
+
+export default function Modal({children, open, className}){
+    const dialog = useRef();
+    useEffect(() => {
+        if(open){
+            dialog.current.showModal();
+        }
+        else{
+            dialog.current.close();
+        }
+    }, [open])
+    return createPortal(
+    <dialog ref={dialog} className={`
+        fixed inset-0 m-auto
+        w-11/12 max-w-md
+        bg-linear-to-br from-stone-900 via-stone-950 to-stone-900
+        border border-teal-500/20 rounded-3xl p-6
+        shadow-2xl text-stone-100
+        backdrop:bg-stone-950/80 backdrop:backdrop-blur-xs
+        ${className}
+      `}>
+      {children}  
+    </dialog>, document.getElementById('modal'));
+}
